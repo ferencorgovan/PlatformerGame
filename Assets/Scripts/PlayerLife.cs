f3e10b74ec3c14de79c5fa466b3f093362542bf1
ferencorgovan <orgovanferike@gmail.com>
2023-11-01 12:57:19 +0100

    improve player death and animations

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -7,10 +7,16 @@ public class PlayerLife : MonoBehaviour
 {
     private Animator anim;
     private Rigidbody2D rb;
+
+    [SerializeField] GameObject player;
+
+
+    Vector2 startPos;
     void Start()
     {
-        anim = GetComponentInChildren<Animator>();
-        rb = GetComponentInChildren<Rigidbody2D>();
+        startPos = transform.position;
+        anim = GetComponent<Animator>();
+        rb = GetComponentInParent<Rigidbody2D>();
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -29,5 +35,12 @@ public class PlayerLife : MonoBehaviour
     private void RestartLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void RespawnPlayer()
+    {
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        player.transform.position = startPos;
+        anim.SetTrigger("respawned");
     }
 }
